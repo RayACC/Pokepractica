627cb26616301e7c616b57e537b434002caed9af
Ray Antonio Cisneros Coria <36940653+RayACC@users.noreply.github.com>
2018-04-03 08:00:44 -0700

    Update Pokedex_data.py
    uso de la funcion open

--- Pokedex_data.py
+++ Pokedex_data.py
@@ -4,5 +4,12 @@ Numero:
 Nombre:
 Descripcion:
   
+funcion open("nombre del archivo","modo")
+modos: w , a , r
+w: crear y escribe archivo
+a: si el archivo existe agrega al final
+r: solo lectura
+
+  
 """""
 
